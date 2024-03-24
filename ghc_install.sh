0be6dd7b6e2773b09b88d0cd85e0847333de7210
Sergey <dream.doomwalker@gmail.com>
2024-03-24 21:03:33 +0300

    fix path

--- ghc_install.sh
+++ ghc_install.sh
@@ -1,4 +1,9 @@
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-ghcup install hls
+source ~/.bashrc
+ghcup install ghc --set recommended
+ghcup install cabal latest
+ghcup install stack latest
+ghcup install hls latest
+cabal update
 #echo 'PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"' >> ~/.bashrc
 echo "$PATH" 
